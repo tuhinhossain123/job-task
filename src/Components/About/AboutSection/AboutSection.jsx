7e363291264d0457241d45edd7038b9993c4a111
tuhinhossain123 <csetuhin55@gmail.com>
2024-11-09 10:00:53 +0600

    bg sidebar add

--- src/Components/About/AboutSection/AboutSection.jsx
+++ src/Components/About/AboutSection/AboutSection.jsx
@@ -1,10 +1,11 @@
 import img1 from "../../../assets/images/Frame 1707478063.png";
+import img2 from "../../../assets/images/top-view-bell-pepper-pepper-sliced-black-bowl-white-horizontal 2.png";
 import AbouSectionTab from "../AbouSectionTab/AbouSectionTab";
 import AboutServices from "../AboutServices/AboutServices";
 
 const AboutSection = () => {
   return (
-    <div className="w-full my-8 lg:my-[120px] px-6">
+    <div className="w-full my-8 lg:my-[120px] px-6 relative">
       <div className="max-w-[1320px] mx-auto ">
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-20">
           <div>
@@ -19,6 +20,9 @@ const AboutSection = () => {
           </div>
         </div>
         <AboutServices></AboutServices>
+      </div>
+      <div className="absolute right-0 bottom-[5%] hidden lg:block">
+        <img src={img2} alt="" />
       </div>
     </div>
   );
