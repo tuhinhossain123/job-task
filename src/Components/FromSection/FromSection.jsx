8bb828f11b88246940fdcf87e51897c94ce2876d
tuhinhossain123 <csetuhin55@gmail.com>
2024-11-11 11:45:04 +0600

    react hook from use

--- src/Components/FromSection/FromSection.jsx
+++ src/Components/FromSection/FromSection.jsx
@@ -1,5 +1,15 @@
+import { useForm } from "react-hook-form";
 import img from "../../assets/images/from-bg.png";
 const FromSection = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div
       className="w-full px-6"
@@ -26,40 +36,63 @@ const FromSection = () => {
           molestie vel, ornare non id blandit netus.
         </p>
         <div className="pt-[40px]">
-          <form className="w-full  lg:w-[50%]">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full  lg:w-[50%]"
+          >
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-[30px]">
-              <div className="form-control w-full bg-none">
+              <div className="form-control w-full">
                 <input
                   type="text"
-                  name="firstName"
-                  className="input bg-transparent border border-[#E5E7EB] w-full text-[14px] font-robotto font-normal px-4 py-3 text-white placeholder:text-white focus:outline-none"
+                  {...register("name", { required: true })}
+                  className="input bg-transparent focus:bg-transparent border border-[#E5E7EB] w-full text-[14px] font-robotto font-normal px-4 py-3 text-white placeholder:text-white focus:outline-none"
                   placeholder="Your Name *"
                 />
+                {errors.name && (
+                  <span className="text-primary font-robotto  text-base">
+                    name is required
+                  </span>
+                )}
               </div>
-              <div className="form-control w-full bg-none">
+              <div className="form-control w-full">
                 <input
-                  type="text"
-                  name="firstName"
+                  type="email"
+                  {...register("email", { required: true })}
                   className="input bg-transparent border border-[#E5E7EB] w-full text-[14px] font-robotto font-normal px-4 py-3 text-white placeholder:text-white focus:outline-none"
                   placeholder="Your Email"
                 />
+                {errors.email && (
+                  <span className="text-primary font-robotto text-base">
+                    email is required
+                  </span>
+                )}
               </div>
             </div>
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-[30px] pt-4 lg:pt-6">
-              <div className="form-control w-full bg-none">
+              <div className="form-control w-full">
                 <input
                   type="date"
-                  name="firstName"
+                  {...register("date", { required: true })}
                   className="omput bg-transparent  border border-[#E5E7EB] w-full text-white placeholder:text-white text-[14px] font-roboto font-normal px-4 py-3 focus:outline-none"
                 />
+                 {errors.date && (
+                  <span className="text-primary font-robotto text-base">
+                    date is required
+                  </span>
+                )}
               </div>
               <div className="form-control w-full text-red-500">
                 <input
                   type="number"
-                  name="firstName"
+                  {...register("number", { required: true })}
                   className="input bg-transparent border border-[#E5E7EB] w-full text-[14px] font-robotto font-normal px-4 py-3 text-white placeholder:text-white focus:outline-none"
                   placeholder="Total People"
                 />
+                  {errors.number && (
+                  <span className="text-primary font-robotto text-base">
+                    number is required
+                  </span>
+                )}
               </div>
             </div>
             <div className="pt-4 lg:pt-6">
@@ -67,10 +100,15 @@ const FromSection = () => {
                 id=""
                 cols="7"
                 rows="7"
-                name="comment"
+                {...register("message", { required: true })}
                 placeholder="Message"
                 className="bg-transparent focus:bg-transparent border border-[#E5E7EB] w-full text-white placeholder:text-white text-[14px] font-roboto font-normal px-4 py-3 focus:outline-none"
               ></textarea>
+               {errors.message && (
+                  <span className="text-primary font-medium">
+                    message is required
+                  </span>
+                )}
             </div>
             <div className="pt-6 lg:pt-8">
               <button className="bg-secondury hover:bg-primary hover:text-white px-[20px] lg:px-6 py-3 lg:py-4 text-black text-base lg:text-lg font-bold ">
